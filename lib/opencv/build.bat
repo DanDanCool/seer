d824e99465a47356429b8fa6b2a06ff69230f4fe
Daniel Sun <sun.daniel@outlook.com>
2023-05-01 19:24:07 +0000

    legacy multi tracking

---
+++ lib/opencv/build.bat
@@ -0,0 +1,1 @@
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_wechat_qrcode=OFF -S . -B bin
--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,118 @@
-#include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/video.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/tracking/tracking_legacy.hpp>
+#include <opencv2/core/ocl.hpp>
+
 using namespace cv;
 using namespace std;
 
-int main(int argc, char **argv) {
-    string filename = "assets/test1.mkv";
+// Convert to string
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+( std::ostringstream() << std::dec << x ) ).str()
 
-    VideoCapture capture(filename);
-    if (!capture.isOpened()){
-        //error in opening the video input
-        cerr << "Unable to open file!" << endl;
-        return 0;
-    }
+Ptr<legacy::Tracker> createTracker(const string& name) {
+	Ptr<legacy::Tracker> tracker;
+	if (name == "BOOSTING")
+		tracker = legacy::TrackerBoosting::create();
+	if (name == "MIL")
+		tracker = legacy::TrackerMIL::create();
+	if (name == "KCF")
+		tracker = legacy::TrackerKCF::create();
+	if (name == "TLD")
+		tracker = legacy::TrackerTLD::create();
+	if (name == "MEDIANFLOW")
+		tracker = legacy::TrackerMedianFlow::create();
+	//if (trackerType == "GOTURN")
+	//	tracker = legacy::TrackerGOTURN::create();
+	if (name == "MOSSE")
+		tracker = legacy::TrackerMOSSE::create();
+	if (name == "CSRT")
+		tracker = legacy::TrackerCSRT::create();
 
-    // Create some random colors
-    vector<Scalar> colors;
-    RNG rng;
-    for(int i = 0; i < 100; i++)
-    {
-        int r = rng.uniform(0, 256);
-        int g = rng.uniform(0, 256);
-        int b = rng.uniform(0, 256);
-        colors.push_back(Scalar(r,g,b));
-    }
+	return tracker;
+}
 
-    Mat old_frame, old_gray;
-    vector<Point2f> p0, p1;
+void randomColors(vector<Scalar>& colors, int n) {
+	RNG rng(0);
+	for (int i = 0; i < n; i++) {
+		colors.push_back(Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
+	}
+}
 
-    // Take first frame and find corners in it
-    capture >> old_frame;
-    cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
-    goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+int main(int argc, char** argv) {
+	// List of tracker types in OpenCV 3.4.1
+	string trackerTypes[8] = { "BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT" };
+	// vector <string> trackerTypes(types, std::end(types));
 
-    // Create a mask image for drawing purposes
-    Mat mask = Mat::zeros(old_frame.size(), old_frame.type());
+	int idx = 0;
+	scanf("%d", &idx);
+	// Create a tracker
+	string trackerType = trackerTypes[idx];
 
-    while(true) {
-        Mat frame, frame_gray;
-        capture >> frame;
+	string filename = "assets/street.mp4";
 
-        if (frame.empty())
-            break;
+	// Read video
+	VideoCapture video(filename);
 
-        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	// Exit if video is not opened
+	if (!video.isOpened()) {
+		cout << "Could not read video file" << endl;
+		return 1;
+	}
 
-        // calculate optical flow
-        vector<uchar> status;
-        vector<float> err;
-        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
-        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15,15), 2, criteria);
-        vector<Point2f> good_new;
+	// Read first frame
+	Mat frame;
+	bool ok = video.read(frame);
 
-        for (int i = 0; i < p0.size(); i++) {
-            // Select good points
-            if(status[i] == 1) {
-                good_new.push_back(p1[i]);
-                // draw the tracks
-                line(mask,p1[i], p0[i], colors[i], 2);
-                circle(frame, p1[i], 5, colors[i], -1);
-            }
-        }
+	// Define initial bounding box
+	vector<Rect> bbox;
 
-        Mat img;
-        add(frame, mask, img);
-        imshow("Frame", img);
-        int keyboard = waitKey(30);
-        if (keyboard == 'q' || keyboard == 27)
-            break;
+	bool showCrosshair = true;
+	bool fromCenter = false;
 
-        // Now update the previous frame and previous points
-        old_gray = frame_gray.clone();
-        p0 = good_new;
-    }
+	selectROIs("MultiTracker", frame, bbox, showCrosshair, fromCenter);
+
+	if (bbox.empty()) return 0;
+
+	vector<Scalar> colors;
+	randomColors(colors, bbox.size());
+
+	Ptr<legacy::MultiTracker> tracker = legacy::MultiTracker::create();
+	for (auto& box : bbox) {
+		tracker->add(createTracker(trackerTypes[idx]), frame, Rect2d(box));
+	}
+
+	while (video.read(frame)) {
+		// Start timer
+		double timer = (double)getTickCount();
+
+		video >> frame;
+		if (frame.empty()) break;
+
+		tracker->update(frame);
+
+		auto& objects = tracker->getObjects();
+		for (int i = 0; i < objects.size(); i++) {
+			rectangle(frame, objects[i], colors[i], 2, 1);
+		}
+
+		// Calculate Frames per second (FPS)
+		float fps = getTickFrequency() / ((double)getTickCount() - timer);
+
+		// Display tracker type on frame
+		putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+		// Display FPS on frame
+		putText(frame, "FPS : " + SSTR(int(fps)), Point(100, 50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+		// Display frame.
+		imshow("MultiTracker", frame);
+
+		// Exit if ESC pressed.
+		int k = waitKey(1);
+		if (k == 27) {
+			break;
+		}
+
+	}
 }
